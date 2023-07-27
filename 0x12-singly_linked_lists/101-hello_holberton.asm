eec1cc7f945792cd265077546e9fe1dd2f699c9a
cobbysneeze12 <cobbysneeze12@gmail.com>
2023-07-27 14:28:33 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+    const char *format = "Hello, Holberton\n";
+    printf("%s", format);
+    return (0);
+}
