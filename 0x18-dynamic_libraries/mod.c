f2a4511735da96f8c9a0d6a9c8f2b8a1624b5dd9
cobbysneeze12 <cobbysneeze12@gmail.com>
2024-03-25 14:58:51 -0700

    1oo-operations.so

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * mod - mod two integers
+ * @a: first int
+ * @b: second int
+ * Return: modified final output
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
